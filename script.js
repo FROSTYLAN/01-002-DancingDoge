2b623353b669416a34073c8efcec325ce4aeadc9
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-12 03:51:57 -0500

    creditos retaxmaster

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("dancing");
+  audio.loop = true;
+  audio.volume = 1;
+
+  const volume = document.getElementById("volume");
+  volume.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      this.children[0].classList.remove("hidden");
+      this.children[1].classList.add("hidden");
+    } else {
+      audio.pause();
+      this.children[0].classList.add("hidden");
+      this.children[1].classList.remove("hidden");
+    }
+  });
+});
